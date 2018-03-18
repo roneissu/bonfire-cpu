da8eed80ab9a07ddd03eb841a5a015999f0a488a
Thomas Hornschuh <thomas@hornschuh-family.de>
2018-03-18 21:03:34 +0100

    White space...

--- rtl/riscv_csr_unit.vhd
+++ rtl/riscv_csr_unit.vhd
@@ -9,9 +9,9 @@
 -- Target Devices:
 -- Tool versions:
 -- Description:
---   Bonfire CPU 
+--   Bonfire CPU
 --   (c) 2016,2017 Thomas Hornschuh
---   See license.md for License 
+--   See license.md for License
 --   Control unit, implements the RISC-V CSR instruction and associated state registers
 --   riscv instruction set decoder
 -- Dependencies:
@@ -42,7 +42,7 @@ entity riscv_control_unit is
        DIVIDER_EN: boolean;
        MUL_ARCH: string;
        MCYCLE_EN : boolean:=true
-      
+
     );
     Port ( op1_i : in  STD_LOGIC_VECTOR (31 downto 0);
            wdata_o : out  STD_LOGIC_VECTOR (31 downto 0);
@@ -58,17 +58,17 @@ entity riscv_control_unit is
 
            mtvec_o : out std_logic_vector(31 downto 2);
            mepc_o  : out std_logic_vector(31 downto 2);
-           
-           sstep_o : out std_logic; -- when '1' trap after first instruction after next eret 
-          
+
+           sstep_o : out std_logic; -- when '1' trap after first instruction after next eret
+
            -- trap info import
            mcause_i : in STD_LOGIC_VECTOR (3 downto 0);
            mepc_i : in std_logic_vector(31 downto 2);
            adr_i  : in std_logic_vector(31 downto 0);
            mtrap_strobe_i : in STD_LOGIC; -- indicates that mcause_i, mepc_i and adr_i  should be registered
            cmd_tret_i : in STD_LOGIC; -- return command
-           
-           -- IRQ Handling 
+
+           -- IRQ Handling
            ext_irq_in : in std_logic_vector(7 downto 0);
            timer_irq_in : in std_logic;
            interrupt_exec_o : out std_logic;
@@ -113,7 +113,7 @@ signal mcause_irq : t_mcause;
 signal m_bonfire : t_bonfire_csr :=  ( '0','0' ) ;
 
 -- Cycle counter
-signal mcycle : std_logic_vector(63 downto 0); 
+signal mcycle : std_logic_vector(63 downto 0);
 
 -- bonfire csr
 
@@ -140,11 +140,11 @@ with csr_offset select
              mscratch   when scratch,
              cause_csr(mcause_31,mcause)  when cause,
              mbadaddr when badaddr,
-             mepc&"00" when epc,        
+             mepc&"00" when epc,
              impvers when impid,
              get_mie(irq_enable) when a_ie,
              get_mip(irq_pending) when a_ip,
-             
+
              (others=>'0') when others;
 
 
@@ -154,13 +154,13 @@ csr_in <= csr_t1 when csr_adr(11 downto 8)=m_stdprefix or csr_adr(11 downto 8) =
           mcycle(31 downto 0) when  csr_adr = a_mcycle else
           mcycle(63 downto 32) when csr_adr = a_mcycleh else
           get_bonfire_csr(m_bonfire) when csr_adr = m_bonfire_csr else
-          (others=>'X'); -- don't care 
+          (others=>'X'); -- don't care
 
 Inst_counter_64Bit: entity work.counter_64Bit PORT MAP(
-		clk_i => clk_i ,
-		reset_i => rst_i,
-		counter_value_o => mcycle
-	);
+    clk_i => clk_i ,
+    reset_i => rst_i,
+    counter_value_o => mcycle
+  );
 
 end generate;
 
@@ -183,24 +183,24 @@ begin
 
 end process;
 
- irq_unit: entity work.riscv_interrupts 
+ irq_unit: entity work.riscv_interrupts
  GENERIC MAP(
    NUM_LOCALINTERUPTS=>ext_irq_in'length-1
  )
  PORT MAP(
-		mie =>mie ,
-		ir_in => irq_enable,
-		ir_out => irq_pending,
-		interrupt_exec_o => interrupt_exec_o,
-		interrupt_ack_i => interrupt_ack_i,
-      mcause_o => mcause_irq,
-		ext_irq_in => ext_irq_in(0), 
-      l_irq_in => ext_irq_in(ext_irq_in'high downto 1),
-		timer_irq_in =>timer_irq_in ,
-      software_irq_in => '0',
-		clk_i => clk_i,
-		rst_i =>rst_i 
-	);
+    mie =>mie ,
+    ir_in => irq_enable,
+    ir_out => irq_pending,
+    interrupt_exec_o => interrupt_exec_o,
+    interrupt_ack_i => interrupt_ack_i,
+    mcause_o => mcause_irq,
+    ext_irq_in => ext_irq_in(0),
+    l_irq_in => ext_irq_in(ext_irq_in'high downto 1),
+    timer_irq_in =>timer_irq_in ,
+    software_irq_in => '0',
+    clk_i => clk_i,
+    rst_i =>rst_i
+  );
 
 
 
@@ -229,33 +229,33 @@ begin
         if we='1' then
           we <= '0';
         end if;
-      
+
         if mtrap_strobe_i='1' then
           if interrupt_ack_i='1' then
               mcause_31 <= '1';
               mcause <= mcause_irq;
-          else 
-             mcause_31 <= '0';          
-             mcause <= '0' & mcause_i;            
-             case mcause_i is 
+          else
+             mcause_31 <= '0';
+             mcause <= '0' & mcause_i;
+             case mcause_i is
                when X"4"|X"6"|X"0" =>
                  mbadaddr <= adr_i;
                when X"2"|X"3" =>
                  mbadaddr <= mepc_i & "00";
-               when others =>  
+               when others =>
              end case;
-          end if;   
+          end if;
           mepc <= mepc_i;
           -- save IE and disable
           mpie <= mie;
-          mie <= '0'; 
-          
+          mie <= '0';
+
         elsif cmd_tret_i='1' then
           mie <= mpie;
-        end if;  
-          
+        end if;
+
         if ce_i='1' then
-         
+
           if csr_adr(11 downto 8)=m_stdprefix then
             l_exception:='0';
             case csr_adr(7 downto 0) is
@@ -274,11 +274,11 @@ begin
                  mcause <= csr_out(mcause'range);
                  mcause_31 <= csr_out(31);
               when badaddr =>
-                 mbadaddr <= csr_out;  
+                 mbadaddr <= csr_out;
               when a_ie =>
-                set_mie(csr_out,irq_enable);                
+                set_mie(csr_out,irq_enable);
               when edeleg|ideleg|a_ip=>
-                -- currently read only              
+                -- currently read only
               when others=>
                  l_exception:='1';
             end case;
@@ -287,7 +287,7 @@ begin
           elsif csr_adr(11 downto 8) = m_roprefix then
                case csr_adr(7 downto 0) is
                  when vendorid|marchid|impid|hartid => l_exception:='0';
-                 when others => l_exception:='1';  
+                 when others => l_exception:='1';
                end case;
           elsif csr_adr=m_bonfire_csr then
               l_exception:='0';
@@ -295,8 +295,8 @@ begin
           else
             l_exception:='1';
           end if;
-          
-          if l_exception = '0'  then 
+
+          if l_exception = '0'  then
             wdata_o <= csr_in;
             we <= '1'; -- Pipeline control, latency one cycle
           else
@@ -304,7 +304,7 @@ begin
           end if;
           exception<=l_exception;
         end if;
-       
+
       end if;
    end if;
 end process;
