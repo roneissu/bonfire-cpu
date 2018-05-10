534f5d9f84d9669933301f38a7d7aafce804c27f
Thomas Hornschuh <thomas@hornschuh-family.de>
2018-05-10 21:41:31 +0200

    reset fix, whitespace cleanup
    Fixes behaviour at reset: The LLI Bus was enabled during reset and was
    fetching the first instruction at the reset address all the time during
    reset was asserted. On deasseration of reset the result of this fetch
    was used instead of making a clean refetch.
    The fix ensures that the bus remains idle during reset assertion and the
    first instruction is fetched after reset deassertion

--- rtl/lxp32_fetch.vhd
+++ rtl/lxp32_fetch.vhd
@@ -6,6 +6,10 @@
 -- Copyright (c) 2016 by Alex I. Kuznetsov
 --
 -- The first stage of the LXP32 pipeline.
+
+-- The Bonfire Processor Project, (c) 2016,2017,2018 Thomas Hornschuh
+--
+-- License: See LICENSE or LICENSE.txt File in git project root.
 ---------------------------------------------------------------------
 
 library ieee;
@@ -13,30 +17,30 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity lxp32_fetch is
-	generic(
-		START_ADDR: std_logic_vector(29 downto 0);
+  generic(
+    START_ADDR: std_logic_vector(29 downto 0);
       USE_RISCV : boolean := false
-	);
-	port(
-		clk_i: in std_logic;
-		rst_i: in std_logic;
-		
-		lli_re_o: out std_logic;
-		lli_adr_o: out std_logic_vector(29 downto 0);
-		lli_dat_i: in std_logic_vector(31 downto 0);
-		lli_busy_i: in std_logic;
-      lli_cc_invalidate_o : out std_logic;
-		
-		word_o: out std_logic_vector(31 downto 0);
-		next_ip_o: out std_logic_vector(29 downto 0);
-		valid_o: out std_logic;
-		ready_i: in std_logic;
-      fence_i_i : in std_logic;
-		
-		jump_valid_i: in std_logic;
-		jump_dst_i: in std_logic_vector(29 downto 0);
-		jump_ready_o: out std_logic
-	);
+  );
+  port(
+    clk_i: in std_logic;
+    rst_i: in std_logic;
+
+    lli_re_o: out std_logic;
+    lli_adr_o: out std_logic_vector(29 downto 0);
+    lli_dat_i: in std_logic_vector(31 downto 0);
+    lli_busy_i: in std_logic;
+    lli_cc_invalidate_o : out std_logic;
+
+    word_o: out std_logic_vector(31 downto 0);
+    next_ip_o: out std_logic_vector(29 downto 0);
+    valid_o: out std_logic;
+    ready_i: in std_logic;
+    fence_i_i : in std_logic;
+
+    jump_valid_i: in std_logic;
+    jump_dst_i: in std_logic_vector(29 downto 0);
+    jump_ready_o: out std_logic
+  );
 end entity;
 
 architecture rtl of lxp32_fetch is
@@ -61,6 +65,7 @@ signal fifo_full: std_logic;
 
 signal jr: std_logic:='0';
 
+
 begin
 
 -- INIT state machine (to initialize all registers)
@@ -80,54 +85,77 @@ lpc_init: if not USE_RISCV generate
 
 process (clk_i) is
 begin
-	if rising_edge(clk_i) then
-		if rst_i='1' then
-			init<='0';
-			init_cnt<=(others=>'0');
-		else
-			if init='0' and ready_i='1' then
-				init_cnt<=init_cnt+1;
-				if init_cnt=X"FF" then
-					init<='1';
-				end if;
-			end if;
-		end if;
-	end if;
+  if rising_edge(clk_i) then
+    if rst_i='1' then
+      init<='0';
+      init_cnt<=(others=>'0');
+    else
+      if init='0' and ready_i='1' then
+        init_cnt<=init_cnt+1;
+        if init_cnt=X"FF" then
+          init<='1';
+        end if;
+      end if;
+    end if;
+  end if;
 end process;
 
+valid_o<=not fifo_empty or not init;
+
 end generate;
+
+bonfire_init: if USE_RISCV generate
+
+process (clk_i) is
+begin
+  if rising_edge(clk_i) then
+    if rst_i='1' then
+      init<='0';
+    else
+      if init='0'  then
+        init <= '1';
+      end if;
+    end if;
+  end if;
+end process;
+
+valid_o<=not fifo_empty;
+
+end generate;
+
+
 
 -- FETCH state machine
 
 process (clk_i) is
 begin
-	if rising_edge(clk_i) then
-		if rst_i='1' then
-			fetch_addr<=START_ADDR;
-			requested<='0';
-			jr<='0';
-			suppress_re<='0';
-		else
-			jr<='0';
+  if rising_edge(clk_i) then
+    if rst_i='1' then
+      fetch_addr<=START_ADDR;
+      requested<='0';
+      jr<='0';
+      suppress_re<='0';
+    else
+      jr<='0';
 -- Suppress LLI request if jump signal is active but will not be processed
 -- in this cycle. Helps to reduce jump latency with high-latency LLI slaves.
 -- Note: gating "re" with "jump_valid_i and not jr" asynchronously would
 -- reduce jump latency even more, but we really want to avoid too large
 -- clock-to-out on LLI outputs.
-			suppress_re<=jump_valid_i and not jr and not next_word;
-			if lli_busy_i='0' then
-				requested<=re and not (jump_valid_i and not jr);
-			end if;
-			if next_word='1' then
-				if jump_valid_i='1' and jr='0' then
-					fetch_addr<=jump_dst_i;
-					jr<='1';
-				else
-					fetch_addr<=std_logic_vector(unsigned(fetch_addr)+1);
-				end if;
-			end if;
-		end if;
-	end if;
+      suppress_re<=jump_valid_i and not jr and not next_word;
+      if lli_busy_i='0' then
+        requested<=re and not (jump_valid_i and not jr);
+      end if;
+      if next_word='1' then
+        if jump_valid_i='1' and jr='0' then
+          fetch_addr<=jump_dst_i;
+          jr<='1';
+        else
+          fetch_addr<=std_logic_vector(unsigned(fetch_addr)+1);
+        end if;
+      end if;
+    end if;
+  end if;
 end process;
 
 next_word<=(fifo_empty or ready_i) and not lli_busy_i and init;
@@ -147,25 +175,25 @@ fifo_din<=fetch_addr&lli_dat_i;
 fifo_re<=ready_i and not fifo_empty;
 
 ubuf_inst: entity work.lxp32_ubuf(rtl)
-	generic map(
-		DATA_WIDTH=>62
-	)
-	port map(
-		clk_i=>clk_i,
-		rst_i=>fifo_rst,
-		
-		we_i=>fifo_we,
-		d_i=>fifo_din,
-		re_i=>fifo_re,
-		d_o=>fifo_dout,
-		
-		empty_o=>fifo_empty,
-		full_o=>fifo_full
-	);
+  generic map(
+    DATA_WIDTH=>62
+  )
+  port map(
+    clk_i=>clk_i,
+    rst_i=>fifo_rst,
+
+    we_i=>fifo_we,
+    d_i=>fifo_din,
+    re_i=>fifo_re,
+    d_o=>fifo_dout,
+
+    empty_o=>fifo_empty,
+    full_o=>fifo_full
+  );
 
 next_ip_o<=fifo_dout(61 downto 32);
 
 word_o<=fifo_dout(31 downto 0) when init='1' else X"40"&std_logic_vector(init_cnt)&X"0000";
-valid_o<=not fifo_empty or not init;
+
 
 end architecture;
